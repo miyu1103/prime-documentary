5faeb369f326a332cbf12e41368b23668608d059
miyu1103 <aab153792@gmail.com>
2026-06-20 22:19:59 +0900

    Add Riley thumbnail candidates

--- remotion/src/riley_index.tsx
+++ remotion/src/riley_index.tsx
@@ -2,16 +2,31 @@ import React from 'react';
 import {Composition, registerRoot} from 'remotion';
 import {BRAND} from './brand';
 import {RileyPremium, rileyPremiumDurationInFrames} from './compositions/RileyPremium';
+import {RileyThumbnail, rileyThumbnailVariants} from './compositions/RileyThumbnails';
 
 const RileyRoot: React.FC = () => (
-  <Composition
-    id="RileyPremium"
-    component={RileyPremium}
-    durationInFrames={rileyPremiumDurationInFrames(BRAND.video.fps)}
-    fps={BRAND.video.fps}
-    width={BRAND.video.width}
-    height={BRAND.video.height}
-  />
+  <>
+    <Composition
+      id="RileyPremium"
+      component={RileyPremium}
+      durationInFrames={rileyPremiumDurationInFrames(BRAND.video.fps)}
+      fps={BRAND.video.fps}
+      width={BRAND.video.width}
+      height={BRAND.video.height}
+    />
+    {rileyThumbnailVariants.map((variant, index) => (
+      <Composition
+        key={variant.id}
+        id={`Riley-${variant.id}`}
+        component={RileyThumbnail}
+        durationInFrames={1}
+        fps={BRAND.video.fps}
+        width={BRAND.thumb.width}
+        height={BRAND.thumb.height}
+        defaultProps={{variantIndex: index}}
+      />
+    ))}
+  </>
 );
 
 registerRoot(RileyRoot);
